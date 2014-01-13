f70799d139e1800e5f0ce1cdb130000ec43e04a2
linux_china <linux_china@hotmail.com>
2014-01-13 22:26:22 +0800

    add loop

--- bin/zk-aria2.py
+++ bin/zk-aria2.py
@@ -2,12 +2,23 @@
 # -*- coding: utf-8 -*-
 import sys
 import os
+import tornado.ioloop
+from kazoo.client import KazooClient
+
+
+def my_func(event):
+    print "fired"
 
 
 def do_main_program():
     app_dir = os.path.dirname(os.path.realpath(__file__ + "/.."))
     os.system("aria2c --conf-path=" + app_dir + "/conf/aria2.conf")
     print "Aria2 Started"
+    zk = KazooClient(hosts='127.0.0.1:2181')
+    zk.start()
+    zk.get_children("/aria2", watch=my_func)
+    # io loop
+    tornado.ioloop.IOLoop.instance().start()
 
 
 def do_stop_program():
